b59e5ecbb64c3a674f72462e881e69297a131009
Amazon Donato <ajd.oliveira@gmail.com>
2021-08-10 17:12:42 -0400

    corrections

--- day_six/script.js
+++ day_six/script.js
@@ -8,22 +8,22 @@ showQuestion();
 document.querySelector('.scoreArea button').addEventListener('click', resetEvent);
 
 // Functions
-function showQuestion() { // Função para exibir a questão
-    if(questions[currentQuestion]) { // Se questão for igual a primeira questão
-        let q = questions[currentQuestion]; // Variavel para diminuir o codigo
+function showQuestion() { 
+    if(questions[currentQuestion]) { 
+        let q = questions[currentQuestion]; 
 
         let pct = Math.floor((currentQuestion / questions.length) * 100);
         document.querySelector('.progress--bar').style.width = `${pct}%`;
  
-        document.querySelector('.scoreArea').style.display = 'none'; // Muda o css da classe scoreArea
-        document.querySelector('.questionArea').style.display = 'block'; // Muda o css da classe questionArea
+        document.querySelector('.scoreArea').style.display = 'none'; 
+        document.querySelector('.questionArea').style.display = 'block'; 
 
-        document.querySelector('.question').innerHTML = q.question; // Adicionar conteudo a classe question
-        let optionsHtml = ''; // Monta a exibição das respotas
+        document.querySelector('.question').innerHTML = q.question; 
+        let optionsHtml = ''; 
         for(let i in q.options) {
-            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span>${q.options[i]}</div>`; // parseInt = Tranforma string em number
+            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span>${q.options[i]}</div>`;
         }
-        document.querySelector('.options').innerHTML = optionsHtml; // Exibe as respostas na tela
+        document.querySelector('.options').innerHTML = optionsHtml; 
     
         document.querySelectorAll('.options .option').forEach(item => {
             item.addEventListener('click', optionClickEvent);
